7bc9504143c1c32a447b522c115ccfb5cfaae82a
Matthew12o <72840122+Matthew12o@users.noreply.github.com>
2021-11-21 17:55:57 -0500

    Repository

--- Python/Environment/DataFeedEngine/SimulationEngine/SimulationEngine.py
+++ Python/Environment/DataFeedEngine/SimulationEngine/SimulationEngine.py
@@ -1,10 +1,21 @@
 from Python.Environment.DataFeedEngine.External_API.DataFeedEngine import DataFeedEngine
+from Python.Environment.DataFeedEngine.SimulationEngine.Simulation_Repository import SimulationRepository
 
 class SimulationEngine(DataFeedEngine):
-    def __init__(self, simulation):
-        self.Simulation = simulation
+    def __init__(self):
+        self.Simulation = []
+        self.SimulationRepository = SimulationRepository()
         super.__init__()
     
+    def getSimulationDate(self, simulation_reference):
+        self.Simulation.append(simulation_reference)
+    
+    def createSimulation(self, simulation_reference, save=True):
+        # Create Simulation
+        sim_data = None # Placeholder
 
-    
-    
+        # Upload Simulation
+        if save:
+            self.SimulationRepository.saveToSimulationRepository(simulation_reference, sim_data)
+
+        return sim_data    
