ed8638b61c146ab5315b2b453203f0d3abe02d0b
Matthew12o <72840122+Matthew12o@users.noreply.github.com>
2021-11-22 21:08:00 -0500

    Update Simulation_Repository.py

--- Python/Environment/DataFeedEngine/SimulationEngine/Simulation_Repository.py
+++ Python/Environment/DataFeedEngine/SimulationEngine/Simulation_Repository.py
@@ -3,11 +3,11 @@ class SimulationRepository:
     def __init__(self):
         self.Database = None
     
-    def saveToSimulationRepository(simulation_id, simulation_data):
+    def saveToSimulationRepository(self, simulation_id, simulation_data):
         # Save Data to a simulation repository
         self.Database
 
-    def getFromSimulationRepository(simulation_id):
+    def getFromSimulationRepository(self, simulation_id):
         # Get simulation data
         self.Database
 
